d2bb93771c8b73aa5d04adafd5de7c521b930108
MatiasCamusso14 <86119793+MatiasCamusso14@users.noreply.github.com>
2023-12-14 20:48:52 -0300

    add views

--- src/app.js
+++ src/app.js
@@ -6,17 +6,29 @@ import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import viewsRouter from './routes/views.router.js';
+
+import handlebars from 'express-handlebars'
+import __dirname from './utils/index.js';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT||8080;
+const PORT = process.env.PORT || 8080;
 const connection = mongoose.connect(process.env.MONGO_URL)
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
+app.use('/', viewsRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/pets', petsRouter);
+app.use('/api/adoptions', adoptionsRouter);
+app.use('/api/sessions', sessionsRouter);
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
